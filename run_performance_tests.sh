b62f5e0f7c6d32e2e81daec614051e4e45094d11
Ian Hamilton <ian.hamilton@dnata.com>
2018-11-26 23:25:41 +0400

    corrected variables

--- run_performance_tests.sh
+++ run_performance_tests.sh
@@ -7,7 +7,7 @@ function build {
 }
 
 function run_tests_in_docker {
-  export PROPERTIES="-DdrupalUser=$RHD_DRUPAL_USER -DdrupalUserPassword=$RHD_DRUPAL_PASSWORD -DbaseUrl=$RHD_PERF_TEST_URL -DmaxDuration=$RHD_MAX_DURATION -DrampUpUsers=$RHD_USER_RAMP -DnoAdmins=$RHD_ADMIN_RAMP"
+  export PROPERTIES="-DdrupalUser=$RHD_DRUPAL_USER -DdrupalUserPassword=$RHD_DRUPAL_PASSWORD -DbaseUrl=$RHD_PERF_TEST_URL -DmaxDuration=$RHD_MAX_DURATION -DrampUpUsers=$RHD_USER_RAMP -DrampUpAdmins=$RHD_ADMIN_RAMP"
   docker-compose -p rhd_perf_testing run --rm --no-deps rhd_perf_testing sbt "; clean; set javaOptions ++= \"${PROPERTIES}\".split(\" \").toSeq ; gatling:testOnly perfTests.simulations.LoadTest"
   echo "\n Completed performance tests. \n"
 }
--- src/test/scala/perfTests/simulations/LoadTest.scala
+++ src/test/scala/perfTests/simulations/LoadTest.scala
@@ -7,8 +7,8 @@ import scala.concurrent.duration._
 class LoadTest extends Simulation with Scenarios {
 
   lazy val maxDuration: Int = System.getProperty("maxDuration").toInt
-  lazy val rampUpUsers: Int = System.getProperty("rampUp").toInt
-  lazy val rampUpAdmins: Int = System.getProperty("noAdmins").toInt
+  lazy val rampUpUsers: Int = System.getProperty("rampUpUsers").toInt
+  lazy val rampUpAdmins: Int = System.getProperty("rampUpAdmins").toInt
 
   setUp(
     users
