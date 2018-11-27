761b9e588ac5dc52669570d29d4672bea8a3320d
Ian Hamilton <ian.hamilton@dnata.com>
2018-11-27 21:14:42 +0400

    added constant users per sec

--- src/test/scala/perfTests/simulations/LoadTest.scala
+++ src/test/scala/perfTests/simulations/LoadTest.scala
@@ -8,15 +8,16 @@ class LoadTest extends Simulation with Scenarios {
 
   lazy val maxDuration: Int = System.getProperty("maxDuration").toInt
   lazy val rampUpUsers: Int = System.getProperty("rampUpUsers").toInt
-  lazy val rampUpAdmins: Int = System.getProperty("rampUpAdmins").toInt
+  lazy val rampUpAdmins: Int = System.getProperty("noAdmins").toInt
 
   setUp(
-    users
-      .inject(rampUsers(rampUpUsers) during (10 seconds)),
-    admins
-      .inject(rampUsers(rampUpAdmins) during (10 seconds))
+    admins.inject(
+      rampUsers(rampUpAdmins) during (60 seconds),
+      constantUsersPerSec(rampUpAdmins) during (maxDuration minutes) randomized),
+    users.inject(
+      rampUsers(rampUpUsers) during (60 seconds),
+      constantUsersPerSec(rampUpUsers) during (maxDuration minutes))
   )
-    .maxDuration(maxDuration minutes)
-    .protocols(httpProtocol)
+    .maxDuration(maxDuration).protocols(httpProtocol)
 
 }
